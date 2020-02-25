1f98272feff96e55844cd6c52bacde96fa2731e8
Ågren <simon.agren@sogeti.com>
2020-02-25 22:38:39 +0100

    Adding some Teams stuff

--- src/bots/bot.ts
+++ src/bots/bot.ts
@@ -1,14 +1,19 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-import { ActivityHandler } from 'botbuilder';
+import { Activity, ActivityHandler, ActivityTypes, Mention, TurnContext} from 'botbuilder';
 
 export class SimonBot extends ActivityHandler {
     constructor() {
         super();
         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
         this.onMessage(async (context, next) => {
-            await context.sendActivity(`You said '${ context.activity.text }'`);
+
+            if (context.activity.channelId === 'msteams') {
+                await this._messageWithMention(context);
+            } else {
+                await context.sendActivity(`You said '${ context.activity.text }'`);
+            }
             // By calling next() you ensure that the next BotHandler is run.
             await next();
         });
@@ -24,4 +29,23 @@ export class SimonBot extends ActivityHandler {
             await next();
         });
     }
+
+    private async _messageWithMention(context: TurnContext): Promise<void> {
+        // Create mention object
+        const mention: Mention = {
+            mentioned: context.activity.from,
+            text: `<at>${context.activity.from.name}</at>`,
+            type: 'mention'
+        };
+
+        // Construct message to send
+        const message: Partial<Activity> = {
+            entities: [mention],
+            text: `${mention.text} You said '${ context.activity.text }'`,
+            type: ActivityTypes.Message
+        };
+
+        await context.sendActivity(message);
+    }
+
 }
